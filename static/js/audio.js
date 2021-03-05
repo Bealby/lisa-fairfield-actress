d18c7bf2cf44cf45f728804525650ff8a3e818d9
Bealby <mgbealby@gmail.com>
2021-03-05 09:17:35 +0000

    Clone url: Lisa Bealby Repo. Flask Set-Up

---
+++ static/js/audio.js
@@ -0,0 +1,10 @@
+// Allows only one audio to play at a time
+
+document.addEventListener('play', function(e){
+    var audios = document.getElementsByTagName('audio');
+    for(var i = 0, len = audios.length; i < len;i++){
+        if(audios[i] != e.target){
+            audios[i].pause();
+        }
+    }
+}, true);
---
+++ static/js/email.js
@@ -0,0 +1,31 @@
+// Script for EmailJS API as well as alerts
+
+function sendMail(contactForm) {
+    emailjs.send("gmail", "murray_bealby", {
+        "from_name": contactForm.name.value,
+        "from_email": contactForm.emailaddress.value,
+        "comment": contactForm.comment.value
+    })
+    .then(
+        function(response) {
+            // Clears contact form after message sent
+            document.getElementById("myForm").reset();
+             // Message Alert for 'Message Sent'
+            swal("Message Sent");
+            console.log("SUCCESS", response);
+            setTimeout(function(){
+            window.location.reload(1);
+            }, 2000);
+        },
+        function(error) {
+            // Message Alert for 'Message Fail'
+            swal("Message Fail", "Please check connection and try again", "error");
+            console.log("FAILED", error);
+            setTimeout(function(){
+            window.location.reload(1);
+            }, 2000);
+        }
+    );
+
+    return false;  // To block from loading a new page
+}
